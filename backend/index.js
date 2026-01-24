61166b803cd5be84cd4e49309fc60390d8a03e32
Hitesh joshi <joshihitesh29@gmail.com>
2026-01-24 20:15:09 +0530

    vercel changes

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,26 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const { connectDB } = require('./config/db.js');
+const enableCors = require('./config/cors.js');
 
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-const userRoutes = require('./routes/userRoutes'); // <--- The only user file we need
-const enableCors = require('./config/cors.js');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
 enableCors(app);
 
-// app.options('*', cors());
-
 connectDB();
 
-// --- USE ROUTES ---
-app.use('/api/users', userRoutes); // Handles Login, Register, AND Profile
+app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
-// (Note: We removed app.use('/api/auth', ...) because it is now redundant)
+// ✅ EXPORT APP FOR VERCEL
+module.exports = app;
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
